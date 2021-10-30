e1069d202cee9279fcd9d1328690a6e50f6e2b1f
luna <lunabright817@gmail.com>
2021-10-30 17:05:37 +0900

    라이브러리 에러 해결

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import CssBaseline from "@mui/material/CssBaseline";
+
 function App() {
-  return <div className="App">hello</div>;
+  // return <div className="App">hello</div>;
+  return (
+    <>
+      <CssBaseline />
+      <div className="App">Hi</div>
+    </>
+  );
 }
 
 export default App;
